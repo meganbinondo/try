cde22d47ac9d6068d8695723cd019738af3f0cbc
meganbinondo <meganbinondo@gmail.com>
2023-10-19 08:54:09 +0800

    revise migration

--- database/migrations/2023_10_19_004020_create_carousel_items_table.php
+++ database/migrations/2023_10_19_004020_create_carousel_items_table.php
@@ -12,15 +12,16 @@ return new class extends Migration {
     {
         Schema::create('carousel_items', function (Blueprint $table) {
             $table->id('carousel_item_id');
+            $table->string('carousel_name')->nullable();
+            $table->string('image_path');
+            $table->string('description')->nullable();
 
             $table->timestamps();
         });
 
         Schema::table('carousel_items', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
-            $table->string('carousel_name')->nullable();
-            $table->string('image_path');
-            $table->string('description')->nullable();
+
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
